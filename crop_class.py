a4c33135d7ccafaaf6462356b53a44c48848e885
31784 <nathans596@gmail.com>
2014-06-17 14:06:08 +0100

    c

--- crop_class.py
+++ crop_class.py
@@ -1,10 +1,10 @@
+import random
+
+
 class Crop:
-    """A generic food grop"""
-
-    #constructor
-    def __init__(self,growth_rate, light_need, water_need):
-        #set the attributes with an initial value
-
+    """Generic Crop"""
+    
+    def __init__(self,growth_rate,light_need,water_need):
         self._growth = 0
         self._days_growing = 0
         self._growth_rate = growth_rate
@@ -12,17 +12,114 @@ class Crop:
         self._water_need = water_need
         self._status = "Seed"
         self._type = "Generic"
+        #These attributes are private and are only accessed from within the class
 
-        #teh above attibutes are prefixed with an underscore to indicate
-        #that they should not be accessed directly from outwith the class
+    def needs(self):
+        return {"light need":self._light_need,"water_need":self._water_need}
+
+    def report(self):
+        return {"type":self._type,"status":self._status,"growth":self._growth,"days growing":self._days_growing}
+
+    def _update_status(self):
+        if self._growth > 15:
+            self._status = "Old"
+        elif self._growth > 10:
+            self._status = "Mature"
+        elif self._growth > 5:
+            self._status = "Young"
+        elif self._growth > 0:
+            self._status = "Seedlng"
+        elif self._growth == 0:
+            self._status = "Seed"
+
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            self._growth += self._growth_rate
+        self._days_growing += 1
+        self._update_status()
+
+
+def auto_grow(crop, days):
+    for day in range(days):
+        light = random.randint(1,10)
+        water = random.randint(1,10)
+        crop.grow(light,water)
+
+
+def manual_grow(crop):
+    valid = False
+    while not valid:
+        try:
+            light = int(input("Please enter a light value (1-10) >>> "))
+            if 1 <= light <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between 1 and 10")
+        except ValueError:
+            print("Value entered not valid - please enter a value between 1 and 10")
+    valid = False
+    while not valid:
+        try:
+            water = int(input("Please enter a water value (1-10) >>> "))
+            if 1 <= water <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between 1 and 10")
+        except ValueError:
+            print("Value entered not valid - please enter a value between 1 and 10")                    
+    crop.grow(light,water)
+
+
+def display_menu():
+    print("1. Grow manually over 1 day")
+    print("2. Grow automatically over 30 days")
+    print("3. Report status")
+    print("0. Exit test program")
+    print()
+
+
+def get_menu_choice():
+    option_valid = False
+    while not option_valid:
+        try:
+            choice = int(input("Please select an option from the above menu >>> "))
+            if 0 <= choice <= 4:
+                option_valid = True
+            else:
+                print("Please enter a valid option")
+        except ValueError:
+            print("Please enter a valid option")
+    return choice
+
+
+def manage_crop(crop):
+    print("This is the crop management program")
+    print()
+    noexit = True
+    while noexit:
+        display_menu()
+        option = get_menu_choice()
+        if option == 1:
+            manual_grow(crop)
+            print()
+        elif option == 2:
+            auto_grow(crop,30)
+            print()
+        elif option == 3:
+            print(crop.report())
+            print()
+        elif option == 0:
+            noexit = False
+            print()
+    print("Thank you for using the crop management program")
+
 
 def main():
-    #instaniate the class
     new_crop = Crop(1,4,3)
-    #test to see whether it works or not
-    print(new_crop._status)
-    print(new_crop._light_need)
-    print(new_crop._water_need)
+    manage_crop(new_crop)
 
-if __name__=="__main__":
+    
+if __name__ == "__main__":
     main()
+    
+
